1a804f2d05ac354d5572c97f988aa758747fac08
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-08-25 18:40:17 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ console.log('%c HI', 'color: firebrick')
 function fetchImages() {
   return fetch("https://dog.ceo/api/breeds/image/random/4")
     .then(resp => resp.json())
-    .then(json => (json))
+    .then(json => renderImages(json))
 }
+
+function renderImages() { 
+  
